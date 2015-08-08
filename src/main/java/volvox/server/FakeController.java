130ba7afbd0dfc5245536e37aad634cb313c591b
Charidan <twilly0@gmail.com>
2015-08-08 14:02:33 -0700

    Properly replaced hibernate with JPA. Now saves entries so ids increment.

--- src/main/java/volvox/server/FakeController.java
+++ src/main/java/volvox/server/FakeController.java
@@ -1,11 +1,14 @@
 package volvox.server;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import volvox.beans.Text;
+import volvox.repository.TextRepository;
 
 @RestController
 public class FakeController {
@@ -13,8 +16,13 @@ public class FakeController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    private TextRepository textRepository;
+
     @RequestMapping("/ping")
     public Text ping(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Text(counter.incrementAndGet(), String.format(template, name));
+        Text text = new Text(String.format(template, name));
+        textRepository.save(text);
+        return text;
     }
 }
--- src/main/setup/text.sql
+++
@@ -1,4 +0,0 @@
-create table `text` (
-  `id` INT(10) NULL DEFAULT NULL,
-  `content` VARCHAR(2000) NULL DEFAULT NULL
-);
