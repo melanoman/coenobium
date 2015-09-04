d7f517073c5a8ff04b7ec114168a3c3443b2efce
Charidan <twilly0@gmail.com>
2015-09-04 12:42:21 -0700

    Updated login page to look nice.

--- src/main/java/volvox/config/WebSecurityConfig.java
+++ src/main/java/volvox/config/WebSecurityConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/assets/css/test.css").permitAll()
+                .antMatchers("/", "/home", "/assets/css/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
