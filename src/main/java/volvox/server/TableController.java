851172b733cb78306cb8c8cba92bcc4fae626ed1
Charidan <twilly0@gmail.com>
2015-09-14 15:12:59 -0700

    fixed imports.

--- src/main/java/volvox/server/TableController.java
+++ src/main/java/volvox/server/TableController.java
@@ -2,7 +2,12 @@ package volvox.server;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 import volvox.beans.GameTable;
 import volvox.repository.GameTableRepository;
