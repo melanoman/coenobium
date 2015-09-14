575ddf2dcb087503b46dd74488f661bd15383a45
melanoman <mel.nicholson@incapturetechnologies.com>
2015-09-14 10:27:07 -0700

    Add Table

--- src/main/java/volvox/repository/GameTableRepository.java
+++ src/main/java/volvox/repository/GameTableRepository.java
@@ -1,7 +1,10 @@
 package volvox.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import volvox.beans.GameTable;
 
 public interface GameTableRepository extends CrudRepository<GameTable, Long> {
+    List<GameTable> findByName(String name);
 }
--- src/main/java/volvox/server/TableController.java
+++ src/main/java/volvox/server/TableController.java
@@ -1,9 +1,10 @@
 package volvox.server;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import volvox.beans.GameTable;
@@ -23,17 +24,32 @@ public class TableController {
     public ModelAndView showTables() {
         ModelAndView mav = new ModelAndView("lobby");
         mav.addObject("tables", tables.findAll());
+        GameTable newTable = new GameTable();
+        newTable.setName("New Table Name");
+        mav.addObject("gameTable", newTable);
         return mav;
     }
 
-    @RequestMapping(value = "/table/add")
-    public ModelAndView addTable(@RequestParam("name") String name) {
+    @RequestMapping(value = "/table/add", method = RequestMethod.POST)
+    public ModelAndView addTable(@ModelAttribute GameTable gameTable) {
+        if (gameTable == null || gameTable.getName() == null || gameTable.getName().length() < 1) {
+            return tableError("Table Name is required");
+        }
+        List<GameTable> old = tables.findByName(gameTable.getName());
+        if (old.size() > 0) return tableError("Table already exists");
+
         GameTable table = new GameTable();
-        table.setName(name);
+        table.setName(gameTable.getName());
         table.setLobbyId(-1L);
         tables.save(table);
         ModelAndView mav = new ModelAndView(("table"));
         mav.addObject("table", table);
         return mav;
+    }
+
+    private ModelAndView tableError(String msg) {
+        ModelAndView error = new ModelAndView("tableError");
+        error.addObject("message", msg);
+        return error;
     }
 }
