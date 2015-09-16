3923b3d9466eb7e04e20b0dab49d849c51d9c948
melanoman <mel.nicholson@incapturetechnologies.com>
2015-09-16 16:36:52 -0700

    Strip away test classes

--- src/main/java/volvox/Main.java
+++ src/main/java/volvox/Main.java
@@ -1,16 +1,10 @@
 package volvox;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import volvox.repository.TextRepository;
 
 @SpringBootApplication
 public class Main {
-
-    @Autowired
-    TextRepository repository;
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
--- src/main/java/volvox/repository/TextRepository.java
+++
@@ -1,7 +0,0 @@
-package volvox.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import volvox.beans.Text;
-
-public interface TextRepository extends CrudRepository<Text, Long> {
-}
