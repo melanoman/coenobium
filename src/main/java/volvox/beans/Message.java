08f3ca687ad82437b70dd5f0be655abc1197fdba
melanoman <mel.nicholson@incapturetechnologies.com>
2015-09-16 18:48:11 -0700

    Fix IDE not to ruin imports with *

--- src/main/java/volvox/beans/Message.java
+++ src/main/java/volvox/beans/Message.java
@@ -1,6 +1,10 @@
 package volvox.beans;
 
-import javax.persistence.*;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Map;
 
 @Entity
