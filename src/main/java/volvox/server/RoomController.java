b6e76add21bd0fb82e26e8c0af89ebc3bb357bb2
Charidan <twilly0@gmail.com>
2015-10-31 21:13:05 -0700

    Implemented button to get new chats from server. Currently hardcoded to get messages after 3 from chat_2.

--- src/main/java/volvox/server/RoomController.java
+++ src/main/java/volvox/server/RoomController.java
@@ -6,6 +6,8 @@ import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
